f44d205dac4baed7d1fb322a3b4aaede9f4d914d
TyedeeGit <79290235+TyedeeGit@users.noreply.github.com>
2024-12-23 10:55:58 -0800

    Fixed `all.hpp` to include `TestResults.hpp` and it's documentation.

--- tests/tester-lib/all.hpp
+++ tests/tester-lib/all.hpp
@@ -3,11 +3,12 @@
  *
  * @file all.hpp
  * @author Gianmarco Lenzi
- * @brief Include all the tester classes.
+ * @brief Include all the tester library headers.
  */
 
 #pragma once
 #include "common.hpp"
 #include "logging.hpp"
 #include "Results.hpp"
+#include "TestResults.hpp"
 #include "Tester.hpp"
